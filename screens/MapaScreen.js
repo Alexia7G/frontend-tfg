9c0067156bade048766538d3064262872c105932
Alexia Gallo <gallo.alexiaa@gmail.com>
2022-12-08 00:05:06 -0300

    modelo pantalla mapa

--- screens/MapaScreen.js
+++ screens/MapaScreen.js
@@ -1,14 +1,43 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { useCallback, useLayoutEffect, useState } from "react";
+import MapView, { Marker } from "react-native-maps";
+import Colores from "../Constantes/colores";
 
 const MapaScreen = () => {
+  //const [selectedLocation, setSelectedLocation] = useState(initialLocation);
+
+  const region = {
+    latitude: -31.4, //define el centro de la pantalla
+    longitude: -64.18, //define el centro de la pantalla
+    latitudeDelta: 0.222, //define cuando contenido además del centro es visible
+    longitudeDelta: 0.1421, //define cuando contenido además del centro es visible
+  };
+
   return (
-    <View>
-      <Text>Mapa</Text>
+    <View style={styles.contenedor}>
+      <MapView style={styles.map} initialRegion={region}>
+        <Marker
+          title="Ubi elegida"
+          coordinate={{
+            latitude: region.latitude,
+            longitude: region.longitude,
+          }}
+        />
+      </MapView>
     </View>
   );
 };
 
 export default MapaScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  map: {
+    flex: 1,
+  },
+  contenedor: {
+    marginTop: 235,
+    height: 510,
+    borderColor: Colores.secundario,
+    borderWidth: 2,
+  },
+});
